eca3f60c72b3e47046734e58b35b4cbffc2576ac
Matheus Ataide <matheusataide95@gmail.com>
2019-04-22 16:01:41 -0700

    Prohibited triples

--- main.cpp
+++ main.cpp
@@ -5,6 +5,36 @@ using namespace std;
 int n;
 vector<string> args;
 vector<vector<double> > weights;
+vector<vector<int> > triples;
+
+int lFunction(int u, int pi_1, int pi_2) {
+    return 1 + (pi_1 * u + pi_2) % n;
+}
+
+void mountTriples() {
+    for (int i = 0; i < n; i++) {
+        triples[i][0] = i + 1;
+
+        int g = lFunction(i, 131, 1031);
+        if (g != triples[i][0]) {
+            triples[i][1] = g;
+        } else {
+            triples[i][1] = 1 + (g % n);
+        }
+
+        int h = lFunction(i, 193, 1093);
+        int x = 1 + (h % n);
+        if (h != triples[i][0] && h != triples[i][1]) {
+            triples[i][2] = h;
+        } else if (x != triples[i][0] && x != triples[i][1]) {
+            triples[i][2] = x;
+        } else {
+            triples[i][2] = 1 + (h + 1) % n;
+        }
+        sort(triples[i].begin(), triples[i].end());
+        cout << triples[i][0] << " " << triples[i][1] << " " << triples[i][2] << endl;
+    }
+}
 
 int main(int argc, char** argv) {
     for (int i = 0; i < argc; i++) {
@@ -12,18 +42,18 @@ int main(int argc, char** argv) {
     }
     if (argc < 2) {
         cout << "Usage: " << args[0] << " <instance>" << endl;
+        return 1;
     } else {
         ifstream fin(argv[1]);
         fin >> n;
-        cout << n << endl;
         weights = vector<vector<double> >(n, vector<double>(n, 0.0));
+        triples = vector<vector<int> >(n, vector<int>(3, 0));
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 fin >> weights[i][j];
-                cout << weights[i][j] << " ";
             }
-            cout << endl;
         }
+        mountTriples();
     }
     return 0;
 }
